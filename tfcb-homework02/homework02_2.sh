9640d30d215853877306be5871265e1341f5ffd2
MitchellKluesner <klues009@umn.edu>
2023-10-10 16:42:54 -0700

    Test

--- tfcb-homework02/homework02_2.sh
+++ tfcb-homework02/homework02_2.sh
@@ -5,17 +5,5 @@ cd question02
 
 while read n; do
   echo "Hello my precious ${n}!!!" > "file${n}.txt"
-<<<<<<< HEAD
 done <../homework02/list.txt
 
-
-# LECTURE_DIR="/Users/kluesner/Desktop/School/MCB536/tfcb_2023-main/lectures/lecture04"
-# echo "My name is ${USER}."
-# echo "My home directory is ${HOME}"
-# echo "The contents of the ${LECTURE_DIR} directory are"
-# ls $LECTURE_DIR
-# mkdir homework02
-# mv list.txt homework02
-=======
-done <../homework02/list.txt
->>>>>>> 2a0a276 (Homework02 Push)
